2d3611a03260c23d1e05bb101bc870b35529fdd9
rameez-dynamo <rameez@dyna.mo>
2018-05-25 16:23:12 +0200

    Added all required api methods

--- src/api/index.js
+++ src/api/index.js
@@ -7,4 +7,46 @@ export default class ApoteketApi {
       return error;
     });
   }
+
+  static addItemToCart(item) {
+    return fetch('http://apoteket-uppgift-fe.ginzburg.it/api/cart', {
+         method: 'GET',
+         headers: new Headers({
+           'X-Key': 'qwerty',
+           'Content-Type': 'application/json',
+         }),
+       body: JSON.stringify(item)
+       }).then(response => {
+            return response.json();
+          }).catch(error => {
+            return error;
+          });
+  }
+
+  static getCart() {
+    return fetch('http://apoteket-uppgift-fe.ginzburg.it/api/cart', {
+         method: 'GET',
+         headers: new Headers({
+           'X-Key': 'qwerty',
+         }),
+       // body: 'A=1&B=2'
+     }).then(response => {
+       return response.json();
+     }).catch(error => {
+       return error;
+     });
+  }
+
+  static clearCart() {
+    return fetch('http://apoteket-uppgift-fe.ginzburg.it/api/cart', {
+         method: 'DELETE',
+         headers: new Headers({
+           'X-Key': 'qwerty',
+         }),
+     }).then(response => {
+       return response.json();
+     }).catch(error => {
+       return error;
+     });
+  }
 }
